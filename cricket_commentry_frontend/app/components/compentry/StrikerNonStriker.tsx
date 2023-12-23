96d9c3406346a1ec931cc8a077a5273787c025c4
geekyasif <mdasif08737@gmail.com>
2023-12-23 15:13:56 +0530

    inputplayer responsive fix

--- cricket_commentry_frontend/app/components/compentry/StrikerNonStriker.tsx
+++ cricket_commentry_frontend/app/components/compentry/StrikerNonStriker.tsx
@@ -8,7 +8,7 @@ function StrikerNonStriker() {
   const { state } = useScoreboard();
   const { nonStriker, striker } = state;
   return (
-    <div className="flex gap-2 my-6 w-full p-3 flex-wrap lg:flex-nowrap">
+    <div className="flex gap-2 my-6 w-full p-3">
       <InputPlayer label="Striker" value={striker} type="set_striker" />
       <InputPlayer
         label="Non Striker"
